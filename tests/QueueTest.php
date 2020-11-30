5ddae00a41c759c56e090bbf0cff8c0cab19e5d4
khaled boussoffara <boussoffarakhaled@gmail.com>
2020-11-30 05:44:49 +0100

    test Queue

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -7,6 +7,29 @@ class QueueTest extends TestCase {
     public function testNewQueueIsEmpty() {
         $queue = new Queue;
         $this->assertEquals(0, $queue->getCount());
+
+        return $queue;
+    }
+
+    /**
+     * @depends testNewQueueIsEmpty
+     *
+     * @param Queue $queue
+     */
+    public function testAnItemIsAddedToTheQueue(Queue $queue) {
+        $queue->push("item");
+        $this->assertEquals(1, $queue->getCount());
+        return $queue;
+    }
+
+    /**
+     * @depends testAnItemIsAddedToTheQueue
+     *
+     * @param Queue $queue
+     */
+    public function testAnItemIsRemovedFromTheQueue(Queue $queue) {
+        $queue->pop();
+        $this->assertEquals(0, $queue->getCount());
     }
 
 }
